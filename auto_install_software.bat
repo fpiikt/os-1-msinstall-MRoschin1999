443564b6fdabbc59223bee41df4264316ff1527e
MRoschin1999 <39657014+MRoschin1999@users.noreply.github.com>
2020-04-09 20:02:15 +0300

    Add files via upload

---
+++ auto_install_software.bat
@@ -0,0 +1,6 @@
+"7z1604-x64.msi" /passive /norestart /le "C:\Users\%username%\Desktop\opersis\7zip-log.txt" TARGETDIR="C:\Program Files\7-Zip"
+"paintnet_4.2.10.exe" /auto TARGETDIR="C:\Program Files\Graphics\Paint" DESKTOPSHORTCUT=1
+"inkscape-0.92.4-x64.msi" /qr /norestart /lw "C:\Users\%username%\Desktop\opersis\inkscape-log.txt" TARGETDIR="C:\Program Files\Graphics\Inkscape"
+"LibreOffice_6.4.2_Win_x86.msi" /passive /norestart /le "C:\Users\%username%\Desktop\opersis\libreoffice-install-log.txt"
+"LibreOffice_6.4.2_Win_x86_helppack_ru.msi" /quiet /forcerestart
+"npp.7.8.1.Installer.x64.exe" /S
